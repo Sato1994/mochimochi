a90492288b65c2c46b622c03609e25c956c6289d
Sato199 <satoru.irie1994@gmail.co.jp>
2024-05-18 22:12:11 +0900

    settings

---
+++ main.go
@@ -0,0 +1,17 @@
+// main.go
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+func main() {
+    r := gin.Default()
+    r.GET("/", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "Hi there!",
+        })
+    })
+    r.Run(":8080")
+}
