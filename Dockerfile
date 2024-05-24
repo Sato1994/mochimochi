620b1bda77d4aae3c8aa8b5968ac50493ba5e16c
Sato199 <satoru.irie1994@gmail.co.jp>
2024-05-25 01:20:38 +0900

    nextjs

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-# Dockerfile
-FROM golang:1.22.3-alpine3.18
-
-WORKDIR /app
-COPY . .
-# ホットリロード air
-RUN go install github.com/cosmtrek/air@latest
-
-RUN go mod download
---
+++ server/main.go
@@ -0,0 +1,31 @@
+// main.go
+package main
+
+import (
+	"mochimochi/model"
+	"mochimochi/route"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "host=db port=5432 user=postgres password=postgres dbname=postgres sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	println("Database connect Success!")
+	migrateSchema(db)
+
+	r := gin.Default()
+	route.InitRouter(r, db)
+
+	r.Run(":8080")
+}
+
+func migrateSchema(db *gorm.DB) {
+	db.AutoMigrate(&model.User{})
+	db.AutoMigrate(&model.ExternalWebService{})
+}
