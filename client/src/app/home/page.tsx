c145a770dc85d1acfe48a3a19b31087d2f293754
Sato199 <satoru.irie1994@gmail.co.jp>
2024-05-26 18:02:31 +0900

    .

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,10 +1,19 @@
 import Button from "@/components/Button";
 import { Container } from "@chakra-ui/react";
+import { HttpClient } from "../repositories/httpClient";
 
 export default function home() {
+
+   const handleButtonClick = async () => {
+    const dom = document.documentElement.outerHTML;
+    const client = new HttpClient()
+    console.log(dom);
+    console.log(await client.get("/todos/1"))
+  };
+
   return (
     <Container>
-      <Button/>
+      <Button handleClick={ handleButtonClick } buttonText="DOMの取得" />
     </Container>
   );
 }
--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -4,17 +4,19 @@ import { Button as _Button } from "@chakra-ui/react";
 
 import React from "react";
 
-const Button = () => {
-  const handleButtonClick = async () => {
-    const dom = document.documentElement.outerHTML;
-    const client = new HttpClient()
-    console.log(dom);
-    console.log(await client.get("/todos/1"))
-  };
+type Props = {
+  handleClick: () => Promise<void>;
+  buttonText: string;
+};
+
+
+const Button = (
+  { handleClick, buttonText }: Props
+) => {
   
   return (
-    <_Button colorScheme="blue" size="md" onClick={handleButtonClick}>
-      ドむ取得するよ
+    <_Button colorScheme="blue" size="md" onClick={handleClick}>
+      { buttonText }
     </_Button>
   );
 };
